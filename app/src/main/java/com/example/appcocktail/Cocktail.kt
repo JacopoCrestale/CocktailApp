18efd70d03cf0ace862a667ebe60e8f9e662fb3e
JacopoCrestale <crestale064@gmail.com>
2025-03-25 20:59:04 +0100

    aggiunta ricerca per ingredienti

--- app/src/main/java/com/example/appcocktail/Cocktail.kt
+++ app/src/main/java/com/example/appcocktail/Cocktail.kt
@@ -7,18 +7,63 @@ data class CocktailList(
 data class Cocktail(
     val idDrink: Int,
     val strDrink: String,
-    val strCategory: String,
-    val strInstructions: String,
-    val strIngredient1: String,
-    val strIngredient2: String,
-    val strIngredient3: String,
-    val strIngredient4: String,
-    val strIngredient5: String,
-    val strMeasure1: String,
-    val strMeasure2: String,
-    val strMeasure3: String,
-    val strMeasure4: String,
-    val strMeasure5: String,
-    val strDrinkThumb: String,
-    val strGlass : String
-)
+    val strCategory: String?,
+    val strInstructions: String?,
+    val strDrinkThumb: String?,
+
+    val strIngredient1: String?,
+    val strIngredient2: String?,
+    val strIngredient3: String?,
+    val strIngredient4: String?,
+    val strIngredient5: String?,
+    val strIngredient6: String?,
+    val strIngredient7: String?,
+    val strIngredient8: String?,
+    val strIngredient9: String?,
+    val strIngredient10: String?,
+    val strIngredient11: String?,
+    val strIngredient12: String?,
+    val strIngredient13: String?,
+    val strIngredient14: String?,
+    val strIngredient15: String?,
+
+    val strMeasure1: String?,
+    val strMeasure2: String?,
+    val strMeasure3: String?,
+    val strMeasure4: String?,
+    val strMeasure5: String?,
+    val strMeasure6: String?,
+    val strMeasure7: String?,
+    val strMeasure8: String?,
+    val strMeasure9: String?,
+    val strMeasure10: String?,
+    val strMeasure11: String?,
+    val strMeasure12: String?,
+    val strMeasure13: String?,
+    val strMeasure14: String?,
+    val strMeasure15: String?,
+
+    val strGlass: String?,
+    val strAlcoholic: String?
+){
+    fun getIngredients(): LinkedHashSet<String> {
+        return linkedSetOf(
+            this.strIngredient1,
+            this.strIngredient2,
+            this.strIngredient3,
+            this.strIngredient4,
+            this.strIngredient5,
+            this.strIngredient6,
+            this.strIngredient7,
+            this.strIngredient8,
+            this.strIngredient9,
+            this.strIngredient10,
+            this.strIngredient11,
+            this.strIngredient12,
+            this.strIngredient13,
+            this.strIngredient14,
+            this.strIngredient15
+        ).filterNotNull().toCollection(LinkedHashSet())
+    }
+}
+
--- app/src/main/java/com/example/appcocktail/MainActivity.kt
+++ app/src/main/java/com/example/appcocktail/MainActivity.kt
@@ -7,36 +7,48 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.appcocktail.ui.theme.AppCocktailTheme
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import androidx.navigation.compose.NavHost
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.example.appcocktail.ui.theme.AppCocktailTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,9 +61,14 @@ class MainActivity : ComponentActivity() {
                     composable("mainMenu") { MainMenu(navController) }
                     composable("cocktailList") { CocktailListScreen(navController) }
                     composable("randomCocktail") { RandomCocktailScreen() }
-                    composable("cocktailDetails/{cocktailId}") {
-                        arg -> val cocktailId = arg.arguments?.getString("cocktailId")?:""
+                    composable("cocktailDetails/{cocktailId}") { arg ->
+                        val cocktailId = arg.arguments?.getString("cocktailId") ?: ""
                         CocktailDetails(cocktailId)
+                    }
+                    composable("searchByIngredient") { SearchByIngredientsScreen(navController) }
+                    composable("cocktailsByIngredient/{ingredients}") { arg ->
+                        val ingredients = arg.arguments?.getString("ingredients") ?: ""
+                        CocktailsListByIngredients(navController, ingredients)
                     }
                 }
             }
@@ -65,8 +82,8 @@ fun MainMenu(navController: NavController) {
         modifier = Modifier
             .padding(16.dp)
             .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Welcome to Cocktail App")
         Button(
@@ -83,6 +100,13 @@ fun MainMenu(navController: NavController) {
         ) {
             Text("RANDOM COCKTAIL")
         }
+        Button(
+            onClick = { navController.navigate("searchByIngredient") },
+            modifier = Modifier
+                .padding(top = 8.dp)
+        ) {
+            Text("INGREDIENT SEARCH")
+        }
     }
 }
 
@@ -91,33 +115,38 @@ fun CocktailListScreen(navController: NavController) {
     val apiService = RetrofitInstance.api
     var cocktails by remember { mutableStateOf<List<Cocktail>>(emptyList()) }
     var searchQuery by remember { mutableStateOf("") }
-
-    LaunchedEffect(Unit) {
-        try {
-            cocktails = emptyList()
-            ('a'..'z').forEach { letter ->
-                val response = apiService.getCocktailsByLetter(letter)
-                cocktails += response.drinks ?: emptyList()
+    val coroutineScope = rememberCoroutineScope()
+    if(cocktails.isEmpty()) {
+        LaunchedEffect(Unit) {
+            coroutineScope.launch {
+                try {
+                    cocktails = emptyList()
+                    ('a'..'z').forEach { letter ->
+                        val response = apiService.getCocktailsByLetter(letter)
+                        cocktails += response.drinks ?: emptyList()
+                    }
+                } catch (e: Exception) {
+                    Log.e("Exception", e.message.toString())
+                }
             }
-        } catch (e: Exception) {
-            Log.e("Exception", e.message.toString())
         }
     }
 
-    val filteredCocktails = cocktails.filter{ it.strDrink.contains(searchQuery, ignoreCase = true) }
+    val filteredCocktails =
+        cocktails.filter { it.strDrink.contains(searchQuery, ignoreCase = true) }
 
     Column {
-        SearchBar(searchQuery) {searchQuery = it}
+        SearchBar(searchQuery) { searchQuery = it }
         LazyColumn {
-            items(filteredCocktails) {
-                cocktail -> CocktailCard(cocktail = cocktail, navController)
+            items(filteredCocktails) { cocktail ->
+                CocktailCard(cocktail = cocktail, navController)
             }
         }
     }
 }
 
 @Composable
-fun SearchBar(query : String, onQueryChanged: (String) -> Unit) {
+fun SearchBar(query: String, onQueryChanged: (String) -> Unit) {
     TextField(
         value = query,
         onValueChange = onQueryChanged,
@@ -126,7 +155,10 @@ fun SearchBar(query : String, onQueryChanged: (String) -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-            shape = RoundedCornerShape(12.dp)
+        shape = RoundedCornerShape(12.dp),
+        leadingIcon = {
+            Icon(Icons.Default.Search, contentDescription = "Search Icon")
+        }
     )
 }
 
@@ -163,19 +195,21 @@ fun CocktailCard(cocktail: Cocktail, navController: NavController, modifier: Mod
 fun CocktailDetails(cocktailId: String) {
     val apiService = RetrofitInstance.api
     var cocktail by remember { mutableStateOf<Cocktail?>(null) }
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(cocktailId) {
-        try {
-            cocktail = apiService.getCocktailById(cocktailId).drinks?.get(0)
-        } catch (e: Exception) {
-            Log.e("Exception", e.message.toString())
+        coroutineScope.launch {
+            try {
+                cocktail = apiService.getCocktailById(cocktailId).drinks?.get(0)
+            } catch (e: Exception) {
+                Log.e("Exception", e.message.toString())
+            }
         }
     }
 
-    if (cocktail == null) {
+    if(cocktail == null) {
         Text(text = "Loading...")
-    } else
-    {
+    } else {
         Column(
             modifier = Modifier
                 .padding(16.dp)
@@ -220,7 +254,7 @@ fun CocktailDetails(cocktailId: String) {
                     cocktail?.strIngredient5 to cocktail?.strMeasure5
                 )
                 ingredients.forEach { (ingredient, measure) ->
-                    if (!ingredient.isNullOrEmpty()) {
+                    if(!ingredient.isNullOrEmpty()) {
                         Text(
                             text = "- $ingredient${measure?.let { " ($it)" } ?: ""}",
                             style = MaterialTheme.typography.bodyMedium,
@@ -258,9 +292,179 @@ fun RandomCocktailScreen() {
             Log.e("Exception", e.message.toString())
         }
     }
-    if(cocktails.isEmpty()){
+    if(cocktails.isEmpty()) {
         Text("Loading")
     } else {
         CocktailDetails(cocktails[0].idDrink.toString())
     }
 }
+
+@Composable
+fun SearchByIngredientsScreen(navController: NavController) {
+    val apiService = RetrofitInstance.api
+    var cocktails by remember { mutableStateOf<List<Cocktail>>(emptyList()) }
+    var searchText by remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    if(cocktails.isEmpty()) {
+        LaunchedEffect(Unit)
+        {
+            coroutineScope.launch {
+                try {
+                    cocktails = emptyList()
+                    ('a'..'z').forEach { letter ->
+                        val response = apiService.getCocktailsByLetter(letter)
+                        cocktails += response.drinks ?: emptyList()
+                    }
+                } catch (e: Exception) {
+                    Log.e("Exception", e.message.toString())
+                }
+            }
+        }
+    }
+
+    val ingredients = LinkedHashSet<String>()
+    for (c in cocktails) ingredients += c.getIngredients()
+
+    var selectedIngredients by remember { mutableStateOf<List<String>>(emptyList()) }
+    val searchedIngredients = ingredients
+        .filter { it.contains(searchText, ignoreCase = true) }
+        .toList()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            // Search bar
+            TextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                label = { Text("Search for ingredients") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                leadingIcon = {
+                    Icon(Icons.Default.Search, contentDescription = "Search Icon")
+                }
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                items(searchedIngredients) { ingredient ->
+                    Column {
+                        IngredientItem(
+                            ingredient = ingredient,
+                            isSelected = selectedIngredients.contains(ingredient),
+                            onSelectionChange = { selected ->
+                                selectedIngredients = if(selected) {
+                                    selectedIngredients + ingredient
+                                } else {
+                                    selectedIngredients - ingredient
+                                }
+                            }
+                        )
+                        HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+                    }
+                }
+            }
+        }
+
+        Button(
+            onClick = {
+                navController.navigate("cocktailsByIngredient/${selectedIngredients.joinToString(",")}")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .height(50.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text(text = "SEARCH", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+
+@Composable
+fun CocktailsListByIngredients(navController: NavController, ingredients: String) {
+    val apiService = RetrofitInstance.api
+    var cocktails by remember { mutableStateOf<List<Cocktail>>(emptyList()) }
+    var searchQuery by remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    if(cocktails.isEmpty()) {
+        LaunchedEffect(Unit) {
+            coroutineScope.launch {
+                try {
+                    cocktails = emptyList()
+                    ('a'..'z').forEach { letter ->
+                        val response = apiService.getCocktailsByLetter(letter)
+                        cocktails += response.drinks ?: emptyList()
+                    }
+                } catch (e: Exception) {
+                    Log.e("Exception", e.message.toString())
+                }
+            }
+        }
+    }
+
+    var selectedIngredients = ingredients.split(",")
+
+    val filteredCocktails = cocktails.filter { cocktail ->
+        selectedIngredients.all { it in cocktail.getIngredients() }
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        SearchBar(searchQuery) { searchQuery = it }
+
+        if(filteredCocktails.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No Cocktails Found",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Gray
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(filteredCocktails) { cocktail ->
+                    CocktailCard(cocktail = cocktail, navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun IngredientItem(ingredient: String, isSelected: Boolean, onSelectionChange: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .clickable { onSelectionChange(!isSelected) },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = ingredient,
+            fontSize = 18.sp,
+            modifier = Modifier.weight(1f) // Pushes Checkbox to the right
+        )
+
+        Checkbox(
+            checked = isSelected,
+            onCheckedChange = { onSelectionChange(it) },
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
